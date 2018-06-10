284ab084105d012993a566ed18ce0471057b46c9
grantharper <grant.e.harper@gmail.com>
2018-06-10 13:34:22 -0400

    add lambda logger

--- src/main/java/org/grantharper/aws/Application.java
+++ src/main/java/org/grantharper/aws/Application.java
@@ -1,13 +1,19 @@
 package org.grantharper.aws;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Application implements RequestHandler<Request,Response>
 {
+
   @Override
   public Response handleRequest(Request input, Context context)
   {
-    return new Response("1", "success");
+    String inputMessage = input.getMessage();
+    LambdaLogger lambdaLogger = context.getLogger();
+    lambdaLogger.log("input=" + inputMessage);
+
+    return new Response("1", "input message: " + inputMessage);
   }
 }
